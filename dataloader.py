c4c82873deac6fc713ce861fb0f878c1fbfe9ba9
Mrugank Dake <mrugankdake@gmail.com>
2024-07-16 13:45:11 -0400

    playing with different kinds of optimizers

--- dataloader.py
+++ dataloader.py
@@ -13,6 +13,9 @@ def set_paths(params):
         # If one of the lab computers with local mount of data server
         p['pRF_data'] = '/d/DATA/data/popeye_pRF/'
         p['orig_data'] = '/d/DATD/datd/pRF_orig/'
+    else: # Set paths on local macbook of Mrugank
+        p['pRF_data'] = '/Users/mrugankdake/Documents/Clayspace/MRI/popeye_pRF/'
+        p['orig_data'] = '/Users/mrugankdake/Documents/Clayspace/MRI/pRF_orig/'
     # else:
         # Set paths on HPC
     p['stimuli_path'] = os.path.join(p['pRF_data'], 'Stimuli')
@@ -31,13 +34,11 @@ def set_paths(params):
     p['pRF_anat'] = os.path.join(p['pRF_data'], subjID, 'anat_T1_brain.nii')
 
     # Copy a folder as a hyperlink
-    p['orig_anat_dir'] = os.path.join(p['orig_data'], subjID, subjID+'anat')
-    p['pRF_anat_dir'] = os.path.join(p['pRF_data'], subjID, subjID+'anat')
-    if not os.path.exists(p['pRF_anat_dir']):
-        shutil.copy(p['orig_anat_dir'], os.path.join(p['pRF_data'], subjID), follow_symlinks=False)
-
-    
-
+    if hostname == 'syndrome' or hostname == 'zod.psych.nyu.edu' or hostname == 'zod':
+        p['orig_anat_dir'] = os.path.join(p['orig_data'], subjID, subjID+'anat')
+        p['pRF_anat_dir'] = os.path.join(p['pRF_data'], subjID, subjID+'anat')
+        if not os.path.exists(p['pRF_anat_dir']):
+            shutil.copy(p['orig_anat_dir'], os.path.join(p['pRF_data'], subjID), follow_symlinks=False)
     return p
 
 def load_stimuli(p):
